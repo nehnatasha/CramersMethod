52305135f8a755aacb5289b4b204556bd62d41d4
Natali <portman088@gmail.com>
2021-10-17 17:37:54 +0300

    refactor code

--- src/main/java/CramerClass.java
+++ src/main/java/CramerClass.java
@@ -1,34 +1,49 @@
 import java.util.ArrayList;
 
 public class CramerClass {
-//
-//    ArrayList<ArrayList<Integer>> matrix = new ArrayList<>(); //дополненная матрица.
-//    ArrayList<ArrayList<Integer>> matrixMain = new ArrayList<>(); //главная матрица - входные данные
-//
-//    public ArrayList<ArrayList<Integer>> initAdditionalArray(String[] matrixRowsStr){
-//        for (String matrixRowStr : matrixRowsStr) {
-//            String[] matrixRowArr = matrixRowStr.split(" ");
-//            ArrayList<Integer> matrixRow = new ArrayList<>();
-//            for (String elStr : matrixRowArr) {
-//                matrixRow.add(Integer.parseInt(elStr));
-//            }
-//            for (int i = 0; i < matrixRowArr.length - 1; i++) {
-//                matrixRow.add(Integer.parseInt(matrixRowArr[i]));
-//            }
-//            matrix.add(matrixRow);
-//        }
-//        return matrix;
-//    }
-//
-//    public ArrayList<ArrayList<Integer>> initMainArray(String[] matrixRowsStr) {
-//        for (String matrixRowStr : matrixRowsStr) {
-//            String[] matrixRowArr = matrixRowStr.split(" ");
-//            ArrayList<Integer> matrixRow = new ArrayList<>();
-//            for (String elStr : matrixRowArr) {
-//                matrixRow.add(Integer.parseInt(elStr));
-//            }
-//            matrixMain.add(matrixRow);
-//        }
-//        return matrixMain;
-//    }
+    /**
+     * Инициализация дополненной матрицы для определения ее дискрименанта
+     *
+     * @param matrix
+     * @return
+     */
+    public static ArrayList<ArrayList<Integer>> createAdditionalMatrix(ArrayList<ArrayList<Integer>> matrix) {
+        ArrayList<ArrayList<Integer>> additionalMatrix = new ArrayList<>(); //главный массив.
+
+        for (ArrayList<Integer> matrixRow : matrix) {
+            ArrayList<Integer> additionalMatrixRow = (ArrayList<Integer>) matrixRow.clone();
+
+            for (int i = 0; i < matrixRow.size() - 1; i++) {
+                additionalMatrixRow.add(matrixRow.get(i));
+            }
+
+            additionalMatrix.add(additionalMatrixRow);
+        }
+
+        return additionalMatrix;
+    }
+
+    public static ArrayList<ArrayList<Integer>> createDeltaMatrix(ArrayList<ArrayList<Integer>> matrix, int n, ArrayList<Integer> result) {
+        ArrayList<ArrayList<Integer>> deltaMatrix = cloneArray(matrix);
+        for (int i = 0; i < matrix.size(); i++) {
+            deltaMatrix.get(i).set(n, result.get(i));
+        }
+
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix.size()-1; j++) {
+                deltaMatrix.get(i).add(deltaMatrix.get(i).get(j));
+            }
+        }
+
+        return deltaMatrix;
+    }
+
+    //глубокое копирование массива
+    private static ArrayList<ArrayList<Integer>> cloneArray(ArrayList<ArrayList<Integer>> array){
+        ArrayList<ArrayList<Integer>> deltaMatrix = new ArrayList<>();
+        for (int i = 0; i < array.size(); i++){
+            deltaMatrix.add((ArrayList<Integer>) array.get(i).clone());
+        }
+        return deltaMatrix;
+    }
 }
